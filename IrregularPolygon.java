ffa48d85be6179767f14ab4d756a2c8741656b3e
samsha8 <sameirasharma@gmail.com>
2025-02-14 23:02:14 +0000

    double

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -1,9 +1,6 @@
-import java.awt.geom.*; // for Point2D.Double
-import java.util.ArrayList; // for ArrayList
-import java.util.concurrent.TimeUnit;
-
-import gpdraw.*; // for DrawingTool
-
+import java.awt.geom.*;  // for Point2D.Double
+import java.util.ArrayList;  // for ArrayList
+import gpdraw.*;  // for DrawingTool
 
 public class IrregularPolygon {
     private ArrayList<Point2D.Double> myPolygon = new ArrayList<Point2D.Double>();
@@ -11,30 +8,55 @@ public class IrregularPolygon {
     // constructor
     public IrregularPolygon() {}
 
-    // public methods
-    public void add(Point2D.Double aPoint)
-    {
-        // TODO: Add a point to the IrregularPolygon.
+    // Add a point to the IrregularPolygon
+    public void add(Point2D.Double aPoint) {
+        myPolygon.add(aPoint);
     }
 
+    // Calculate the perimeter of the polygyon
     public double perimeter() {
-        // TODO: Calculate the perimeter.
-        return 3.14;
+        double perimeter = 0.0;
+        int n = myPolygon.size();
+
+        for (int i = 0; i < n; i++) {
+            Point2D.Double current = myPolygon.get(i);
+            Point2D.Double next = myPolygon.get((i + 1) % n);  // Wrap around to the first point
+            perimeter += current.distance(next);
+        }
+        return perimeter;
     }
 
+    // Calculate the area using the Shoelace formula
     public double area() {
-        // TODO: Calculate the area.
-        return 0.0;
+        double sum1 = 0.0;  // Sum of x_i * y_(i+1)
+        double sum2 = 0.0;  // Sum of y_i * x_(i+1)
+        int n = myPolygon.size();
+
+        for (int i = 0; i < n; i++) {
+            Point2D.Double current = myPolygon.get(i);
+            Point2D.Double next = myPolygon.get((i + 1) % n);  // Wrap around to the first point
+            sum1 += current.x * next.y;
+            sum2 += current.y * next.x;
+        }
+        return Math.abs(0.5 * (sum1 - sum2));
     }
 
-    public void draw()
-    {
-        // Wrap the DrawingTool in a try/catch to allow development without need for graphics.
+    // Draw the polygon using DrawingTool
+    public void draw() {
         try {
-            // TODO: Draw the polygon.
-            // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
             DrawingTool pen = new DrawingTool(new SketchPad(500, 500));
-            pen.move(50, 50);
+            if (myPolygon.size() > 0) {
+                pen.up();
+                Point2D.Double firstPoint = myPolygon.get(0);
+                pen.move(firstPoint.x, firstPoint.y);
+                pen.down();
+
+                for (int i = 1; i < myPolygon.size(); i++) {
+                    Point2D.Double nextPoint = myPolygon.get(i);
+                    pen.move(nextPoint.x, nextPoint.y);
+                }
+                pen.move(firstPoint.x, firstPoint.y);  // Close the polygon
+            }
         } catch (java.awt.HeadlessException e) {
             System.out.println("Exception: No graphics support available.");
         }
